79af418b6a23337a8673f82f9d98898ea3aa5bd7
unknown <Admin@IHR80PBF451.continental.edu.pe>
2019-11-12 21:42:30 -0500

    Segundo Cambio

--- app/src/main/java/com/example/jucia/MainActivity.java
+++ app/src/main/java/com/example/jucia/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jucia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume(){ // Este codigo se ejecuta antes de todo.
+        super.onResume();
+        // En este punto la aplicacion ya cargo.
+        // This en un activity ;
+        Toast.makeText(this,
+                "Aplicacion Iniciada",
+                Toast.LENGTH_LONG).show();
+        // Toast.LENGTH_SHORT -- menos tiempo;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,
+                "Aplicacion Stopeada",
+                Toast.LENGTH_LONG).show();
+
     }
 }
